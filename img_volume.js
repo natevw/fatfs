e7ba3149910bbbf432465e132bbc45765807de09
Nathan Vander Wilt <nate@calftrail.com>
2014-05-24 11:13:08 -0700

    fix read-only logic

--- img_volume.js
+++ img_volume.js
@@ -7,6 +7,6 @@ exports.createDriverSync = function (path, opts) {
         fd = fs.openSync(path, (ro) ? 'r' : 'r+');
     return {
         read: fs.read.bind(fs, fd),
-        write: (ro) ? fs.write.bind(fs, fd) : null
+        write: (ro) ? null : fs.write.bind(fs, fd)
     };
 };
